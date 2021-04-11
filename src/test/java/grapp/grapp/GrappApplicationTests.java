582305d65ab6e23076dee1da2359f6b3ac3d7785
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-04-11 13:14:49 +0200

    Junit TEST added

--- src/test/java/grapp/grapp/GrappApplicationTests.java
+++ src/test/java/grapp/grapp/GrappApplicationTests.java
@@ -1,5 +1,8 @@
 package grapp.grapp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,9 @@ class GrappApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Im testing");
+		assertTrue(true);
+		assertEquals(1,2);
 	}
 
 }
