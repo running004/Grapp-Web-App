dbdfb19ed280c3431978bd0b78bcf4e5032fb95b
Termobot <termostato_7@hotmail.com>
2021-04-24 20:36:58 +0200

    datasource singleton

--- src/main/java/grapp/grapp/Connect.java
+++ src/main/java/grapp/grapp/Connect.java
@@ -3,8 +3,8 @@ package grapp.grapp;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import java.sql.SQLException;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Autowired;
+/*import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;*/
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -14,15 +14,14 @@ public class Connect {
     @Value("${spring.datasource.url}")
     private String dbUrl;
 
-    @Autowired
     private DataSource dataSource;
 
-    public DataSource getDataSource(){
+    public DataSource getDataSource()throws SQLException{
+      if(dataSource == null) dataSource = dataSource();
       return dataSource;
     }
 
 
-    @Bean
     public DataSource dataSource() throws SQLException {
       if (dbUrl == null || dbUrl.isEmpty()) {
         return new HikariDataSource();
