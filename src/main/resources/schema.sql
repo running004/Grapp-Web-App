b9935d48e6534519978b63448d8e93e69efd4e85
Termobot <termostato_7@hotmail.com>
2021-05-04 12:59:04 +0200

    schema.sql actualizado

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE public.prendas
     nombre character varying(50) NOT NULL,
     imgurl character varying(20),
     descripcion character varying(280),
-    propietario character varying(10) NOT NULL,
+    propietario character varying(40) NOT NULL,
     CONSTRAINT prendas_pkey PRIMARY KEY (propietario, nombre),
     CONSTRAINT prendas_propietario_fkey FOREIGN KEY (propietario)
         REFERENCES public.usuarios (email) MATCH SIMPLE
@@ -15,7 +15,7 @@ CREATE TABLE public.prendas
 
 CREATE TABLE public.usuarios
 (
-    email character varying(10) NOT NULL,
+    email character varying(40) NOT NULL,
     "contraseña" character varying(64),
     CONSTRAINT usuarios_pkey PRIMARY KEY (email),
     CONSTRAINT usuarios_email_key UNIQUE (email)
